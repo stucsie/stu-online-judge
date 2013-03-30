4ddcdc18485361729aa20ca19a8826cc8c45b59e
afutseng <afutseng@gmail.com>
2013-03-30 15:56:46 +0800

    Display errors in development environment

--- init.php
+++ init.php
@@ -4,6 +4,11 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     include 'vendor/autoload.php';
 }
 
+error_reporting(E_ALL ^ E_NOTICE ^ E_STRICT);
+$flag = (preg_match('/\.test\./i', $_SERVER["SERVER_NAME"]))
+    ? 'On' : 'Off';
+ini_set('display_errors', $flag);
+
 Pix_Controller::addCommonHelpers();
 Pix_Partial::setTrimMode(true);
 Pix_Partial::addCommonHelpers();
