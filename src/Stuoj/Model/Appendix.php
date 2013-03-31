7104a76b65cc54e3350b12add3d981f277c2b232
ali <s11113151@stu.edu.tw>
2013-03-31 20:13:47 +0800

    add appendix model

---
+++ src/Stuoj/Model/Appendix.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Stuoj\Model;
+
+/**
+ * AppendixRow
+ *
+ * @uses Pix_Table_Row
+ */
+class AppendixRow extends \Pix_Table_Row
+{
+    public function preInsert()
+    {
+        $this->created_at = time();
+    }
+
+    /**
+     * update
+     *
+     * @param array $data
+     * @access public
+     * @return void
+     */
+    public function update($data)
+    {
+        $table = Appendix::getTable();
+        $columns = array_keys($table->_columns);
+        $update_data = [];
+
+        foreach ($columns as $col) {
+            if (isset($data[$col])) {
+                $update_data[$col] = trim($data[$col]);
+            }
+        }
+
+        parent::update($update_data);
+    }
+}
+
+/**
+ * Appendix
+ *
+ * @uses Pix_Table
+ */
+class Appendix extends \Pix_Table
+{
+    public $_name = 'Appendix';
+    public $_rowClass = '\\Stuoj\\Model\\AppendixRow';
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function init()
+    {
+        $this->_primary = 'id';
+
+        $this->_columns['id'] = ['type' => 'int', 'size' => 10, 'auto_increment' => true];
+        $this->_columns['problem_id'] = ['type' => 'int', 'size' => 10, 'default' => ''];
+        $this->_columns['file_name'] = ['type' => 'varchar', 'size' => 255, 'default' => ''];
+        $this->_columns['create_at'] = ['type' => 'int', 'size' => 11,'default' => ''];
+				
+				$this->_relations['problem_id'] = array(
+						'rel' => 'has_one',
+						'type' => 'problem',
+						'foreign_key' => 'id',
+						'delete' => true
+				);
+    }
+
+    /**
+     * add
+     *
+     * @param array $data
+     * @static
+     * @access public
+     * @return void
+     */
+    public static function add($data)
+    {
+        $insert_data = [
+            'problem_id' => $data['problem_id'],
+            'file_name' => $data['file_name']
+        ];
+
+        return self::insert($insert_data);
+    }
+}
