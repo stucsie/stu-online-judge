92a21f5b08662d2e008d5535319114296aacd100
afutseng <afutseng@gmail.com>
2013-03-31 22:33:43 +0800

    Add AlertException and views

---
+++ src/Stuoj/Exception/AlertException.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Stuoj\Exception;
+
+/**
+ * AlertException
+ *
+ */
+class AlertException extends \Exception
+{
+    /**
+     * url
+     *
+     * @var string
+     * @access protected
+     */
+    protected $url;
+
+    /**
+     * __construct
+     *
+     * @param string $message
+     * @param string $url
+     * @access public
+     * @return void
+     */
+    public function __construct($message, $url = null)
+    {
+        parent::__construct($message);
+        $this->_url = $url;
+    }
+
+    /**
+     * getURL
+     *
+     * @access public
+     * @return string
+     */
+    public function getURL()
+    {
+        return $this->url;
+    }
+}
