f61b2a584c8d5a18373a0218aee64efeeaf4e8b2
afutseng <afutseng@gmail.com>
2013-04-08 20:58:55 +0800

    Implement basic solveAction

--- src/Stuoj/Controller/ProblemController.php
+++ src/Stuoj/Controller/ProblemController.php
@@ -3,6 +3,7 @@
 namespace Stuoj\Controller;
 
 use Stuoj\Model\Problem;
+use Stuoj\Model\Solution;
 
 class ProblemController extends BaseController
 {
@@ -39,12 +40,27 @@ class ProblemController extends BaseController
      */
     public function solveAction()
     {
+        $this->checkLogin();
         $pid = $this->segment(1);
         $p = Problem::find($pid);
         if (! $p) {
             throw new AlertException('沒有這個題目', '/problem/');
         }
 
+        if ($this->isPost()) {
+            $code  = isset($_POST['source_code'])  ? $_POST['source_code'] : '';
+            $data = [
+                'problem_id' => $pid,
+                'user_id' => $this->getUser()->id,
+                'language' => 1,
+                'source_code' => $code,
+                'execute_time' => 0
+            ];
+            $solution = Solution::add($data);
+            $args = ['code' => $code, 'solution_id' => $solution->id];
+            \Resque::enqueue('default', 'Stuoj\Job\JudgeJob', $args);
+            $this->noview();
+        }
         $this->view->problem = $p;
 
     }
